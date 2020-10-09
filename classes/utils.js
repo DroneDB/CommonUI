558f335a7180f87c2cf3fdf557edfea93cc2907d
Piero Toffanin <pt@masseranolabs.com>
2020-10-09 16:19:04 -0400

    Moved function to common module

--- classes/utils.js
+++ classes/utils.js
@@ -1,5 +1,12 @@
-export default{
-    clone: function(obj){
-        return JSON.parse(JSON.stringify(obj));
-    }
+export function clone(obj){
+    return JSON.parse(JSON.stringify(obj));
+}
+
+export function bytesToSize(bytes, decimals = 2){
+    if(bytes == 0) return '0 byte';
+    var k = 1000; // or 1024 for binary
+    var dm = decimals || 3;
+    var sizes = ['bytes', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
+    var i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
--- components/Properties.vue
+++ components/Properties.vue
@@ -38,6 +38,7 @@
 
 <script>
 import Window from './Window.vue';
+import { bytesToSize } from '../classes/utils';
 import ddb from 'ddb';
 
 export default {
@@ -69,14 +70,7 @@ export default {
           }
       },
 
-      bytesToSize(bytes, decimals = 2){
-        if(bytes == 0) return '0 byte';
-        var k = 1000; // or 1024 for binary
-        var dm = decimals || 3;
-        var sizes = ['bytes', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
-        var i = Math.floor(Math.log(bytes) / Math.log(k));
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-      },
+      bytesToSize,
 
       sumSizes(files){
         let sum = 0;
