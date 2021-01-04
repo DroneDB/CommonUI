32538831ccd3ebe6b41fe4d2d43d9106f6c1be07
Piero Toffanin <pt@masseranolabs.com>
2021-01-04 14:30:21 -0500

    Add support for ddb:// tile URLs

--- classes/olHybridXYZ.js
+++ classes/olHybridXYZ.js
@@ -6,7 +6,7 @@ import ddb from 'ddb';
 // from internet and file:// URLs via
 // DDB tiling
 
-function genTileLoadFunction(filePath, minZoom){
+function genTileFSLoadFunction(filePath, minZoom){
     return (tile, src) => {
         tile.setState(TileState.LOADING);
         const { tileCoord } = tile;
@@ -27,9 +27,36 @@ function genTileLoadFunction(filePath, minZoom){
     };
 }
 
+function genTileDDBLoadFunction(ddbUri, minZoom){
+    const { registryUrl, org, ds, path } = parseUri(ddbUri);
+    const dataset = new ddb.Registry(registryUrl).Organization(org).Dataset(ds);
+            
+    return (tile, src) => {
+        tile.setState(TileState.LOADING);
+        const { tileCoord } = tile;
+        let [ tz, tx, ty ] = tileCoord;
+    
+        if (minZoom !== undefined && tz < minZoom){
+            // Do not load this one
+            tile.setState(TileState.EMPTY);
+        }else{
+            tile.getImage().onload = () => {
+                tile.setState(TileState.LOADED);
+            };
+            tile.getImage().onerror = e => {            
+                console.error(e);
+                tile.setState(TileState.ERROR);
+            };
+            tile.getImage().src = dataset.tileUrl(path, tz, tx, ty);
+        }
+    };
+}
+
 class HybridXYZ extends XYZ{
     constructor(opt_options){
         const isFS = opt_options.url && opt_options.url.startsWith("file://");
+        const isDDB = opt_options.url && ddb.utils.isDDBUri(opt_options.url);
+
         let minZoom = opt_options.minZoom;
         if (isFS){
             opt_options.tileSize = 256;
@@ -41,7 +68,9 @@ class HybridXYZ extends XYZ{
         super(opt_options);
 
         if (isFS){
-            this.tileLoadFunction = genTileLoadFunction(opt_options.url, minZoom).bind(this);
+            this.tileLoadFunction = genTileFSLoadFunction(opt_options.url, minZoom).bind(this);
+        }else if (isDDB){
+            this.tileLoadFunction = genTileDDBLoadFunction(opt_options.url, minZoom).bind(this);
         }
     }
 };
