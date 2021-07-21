e5ae6e7db78a1967a5aa944ca51432bb635160c3
Piero Toffanin <pt@masseranolabs.com>
2021-07-21 11:50:37 -0400

    Loading point cloud in potree working

--- components/Potree.vue
+++ components/Potree.vue
@@ -6,7 +6,9 @@
             <i class="icon circle notch spin" />
         </div>
 
-        <div ref="container" class="potree-container">
+        <div class="potree-container" :class="{loading}">
+            <div id="potree_render_area" ref="container"></div>
+            <div id="potree_sidebar_container"> </div>
         </div>
     </div>
 </template>
@@ -23,6 +25,7 @@ export default {
   props: ['files'],
   data: function(){
       return {
+          error: "",
           loading: false,
           loaded: false
       };
@@ -40,9 +43,51 @@ export default {
             // this.map.updateSize();
         });
       },
+      reloadPointClouds: async function(){
+        if (!this.viewer) return;
+
+        if (!this.reloadingPointClouds){
+            this.reloadingPointClouds = true;
+            try{
+                console.log(this.viewer);
+                // this.viewer.reset();
+                // TODO: remove all point clouds
+
+                const pointCloudFiles = this.files.filter(f => f.entry.type === ddb.entry.type.POINTCLOUD);
+                await Promise.all(pointCloudFiles.map(this.addPointCloud));
+                this.viewer.fitToScreen();
+            }catch(e){
+                this.error = e.message;
+            }finally{
+                this.reloadingPointClouds = false;
+            }
+        }
+      },
+      addPointCloud: async function(file){
+          return new Promise((resolve, reject) => {
+            const [dataset, path] = ddb.utils.datasetPathFromUri(file.path);
+
+            // TODO: get EPT path for point cloud
+            // dataset.downloadUrl(path)
+
+            Potree.loadPointCloud("/sample/ept.json", file.label, e => {
+                if (e.type == "loading_failed"){
+                    reject("Cannot load " + file);
+                    return;
+                }
+
+                console.log("LOADED!");
+            
+                this.viewer.scene.addPointCloud(e.pointcloud);
+                e.pointcloud.material.size = 1;
+
+                resolve();
+            });
+          });
+      },
       onTabActivated: async function(){
           // Load!
-          if (!this.loaded){
+          if (!this.loaded && !this.loading){
               try{
                 this.loading = true;
                 // await loadResources("/potree/libs/openlayers3/ol.css");
@@ -67,7 +112,6 @@ export default {
                 await loadResources("/potree/libs/plasio/js/laslaz.js");
 
                 let viewer = new Potree.Viewer(this.$refs.container);
-                this.viewer = viewer;
                 viewer.setEDLEnabled(true);
                 viewer.setFOV(60);
                 viewer.setPointBudget(1*1000*1000);
@@ -75,7 +119,7 @@ export default {
                 viewer.loadGUI(() => {
                     viewer.setLanguage('en');
                     $("#menu_tools").next().show();
-                    viewer.toggleSidebar();
+                    // viewer.toggleSidebar();
                 });
 
                 viewer.scene.scene.add( new THREE.AmbientLight( 0x404040, 2.0 ) ); // soft white light );
@@ -85,25 +129,14 @@ export default {
                 directional.position.z = 99999999999;
                 viewer.scene.scene.add( directional );
 
-                Potree.loadPointCloud("/sample/ept.json", "Point Cloud", e => {
-                    if (e.type == "loading_failed"){
-                        this.error = "Could not load point cloud"; // TODO
-                        return;
-                    }
-                    console.log(e);
-                
-                    let scene = viewer.scene;
-                    scene.addPointCloud(e.pointcloud);
-                    e.pointcloud.material.size = 1;
-
-                    viewer.fitToScreen();
-                });
-
                 this.loaded = true;
+                this.viewer = viewer;
+
+                this.reloadPointClouds();
               }catch(e){
                 this.error = e.message;
               }finally{
-                  this.loading = false;
+                this.loading = false;
               }
           }
       }
@@ -124,9 +157,10 @@ export default {
                 if (newVal.length !== oldVal.length || 
                     newVal[0] !== oldVal[0] || 
                     newVal[newVal.length - 1] !== oldVal[oldVal.length - 1]){
-                //    this.reloadFileLayers();
+                //    this.reloadPointClouds();
                 }else{
                     // Just update (selection change)
+                    // TODO
                 }
             }, 5);
         }
@@ -150,10 +184,140 @@ export default {
         }
     }
     .potree-container{
-        margin-top: 1px;
+        &.loading{
+            visibility: hidden;
+        }
+        margin: 1px 50px 0 0;
         display: flex;
         width: 100%;
         height: 100%;
+        background: rgb(79,79,79);
+        background: -moz-radial-gradient(center, ellipse cover, rgba(79,79,79,1) 0%, rgba(22,22,22,1) 100%);
+        background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%,rgba(79,79,79,1)), color-stop(100%,rgba(22,22,22,1)));
+        background: -webkit-radial-gradient(center, ellipse cover, rgba(79,79,79,1) 0%,rgba(22,22,22,1) 100%);
+        background: -o-radial-gradient(center, ellipse cover, rgba(79,79,79,1) 0%,rgba(22,22,22,1) 100%);
+        background: -ms-radial-gradient(center, ellipse cover, rgba(79,79,79,1) 0%,rgba(22,22,22,1) 100%);
+        background: radial-gradient(ellipse at center, rgba(79,79,79,1) 0%,rgba(22,22,22,1) 100%);
+    	position: relative;
+    	padding: 0;
+
+        #potree_render_area > canvas{
+            width: 100% !important;
+            height: 100% !important;
+        }
+    }
+
+    .dg{
+	    &.main{
+		    position: absolute;
+		    right: 0px;
+	    }
+	    .c{
+	    	select{
+	    		color: black;
+	    	}
+	    }
+    }
+
+
+    /* Potree specific */
+    #potree_map{
+        position: absolute; 
+        left: 50px; 
+        top: 50px; 
+        width: 400px; 
+        height: 400px;
+        display: none
+    }
+
+    #potree_menu{
+        input{
+            color: buttontext;
+        }
+        legend{
+            display: initial !important;
+            width: initial !important;
+            padding: initial !important;
+            margin-bottom: initial !important;
+            font-size: inherit !important;
+            line-height: initial !important;
+            color: inherit !important;
+            border: initial !important;
+            border-bottom: initial !important;
+        }
+
+        #show_2d_profile{
+            color: initial !important;
+        }
+    }
+
+    #potree_map_header{
+        position: absolute; 
+        width: 100%; 
+        height: 25px; 
+        top: 0px; 
+        background-color: rgba(0,0,0,0.5); 
+        z-index: 1000;
+        border-top-left-radius: 3px; 
+        border-top-right-radius: 3px;
+    }
+
+    #potree_map_content{
+        position: absolute; 
+        z-index: 100; 
+        top: 25px; 
+        width: 100%; 
+        height: calc(100% - 25px); 
+        border: 2px solid rgba(0,0,0,0.5); 
+        box-sizing: border-box;
+    }
+
+    #potree_sidebar_container{
+        overflow-y: auto;
+        background-color: #19282c;
+
+        a{
+            color: #111;
+        }  
+
+        #sidebar_root{
+            width: 300px;
+
+            .pv-menu-list{
+                padding-right: 12px;
+
+                .divider{
+                    padding: 10px 0px 15px 0px;
+                }
+
+                a{
+                    color: #8Aa1c4;
+                } 
+            }
+
+            .measurement-panel-remove:hover{
+                cursor: pointer;
+            }
+
+            position: absolute; 
+            min-height: 100%; 
+            height: 100%;
+
+            .potree_sidebar_brand{
+                display: flex; 
+                flex-direction: row;
+            }
+
+            #potree_version_number{
+                color: #9AA1A4; 
+                font-size: 80%; 
+                font-weight: 100;
+            }
+        }
+    }
+
+    #profile_window{
+        z-index: 999999999999 !important;
     }
 }
 </style>
